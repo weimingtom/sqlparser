3136f431a84d83f5536642d53977bcc2b6eaca8c
xmjiandeh <xmjiandeh@b602a14a-fd2a-0410-a813-abf9b5d94822>
2007-07-24 08:10:19 +0000

    修改表比较字段没有在[来自]出现问题

--- trunk/src/parser/sybase/SybaseIQ12TreeParser.java
+++ trunk/src/parser/sybase/SybaseIQ12TreeParser.java
@@ -501,6 +501,10 @@ public SybaseIQ12TreeParser() {
 					
 							tableCompare.addTableModel1(tableModel1);
 							tableCompare.addTableModel2(tableModel2);
+							t1 = new TableListModel();
+							t1.addTable(tableModel1);
+							t1.addTable(tableModel2);
+							tableCompare.addTableListModel(t1);
 							tableCompare.setCompareMethod(method);
 							tableCompare.setSearchCondition(cond);
 							model = tableCompare;
